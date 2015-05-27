bb18782f002fe98788c5eb72567cdf8c05c00306
Gerardo Manuel Muñoz Illesca <gerardo.manuel3@gmail.com>
2015-05-27 02:40:19 -0300

    arreglo espaciador comentario
    nada del otro mundo

--- db/sql.php
+++ db/sql.php
@@ -4,53 +4,53 @@ include '../session.php';
 $caso = $_REQUEST['caso'];
 
 #echo "Error Codigo Caso: ".$caso."<br>";
-/*	
+/*
 	=========================CUENTAS USUARIOS=======================
-					   | Crear | Comprobar | Modificar |  Eliminar |
+					   				 | Crear | Comprobar | Modificar |  Eliminar |
 	-------------------|-------|-----------|-----------|-----------|
 	Login              |  ---  |    001    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+	-------------------|-------|-----------|-----------|-----------|
 	Logout             |  ---  |    002    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Registro           |  010  |    ---    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Activar Mail       |  ---  |    ---    |    011    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Usuario Existe?    |  ---  |    012    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Mail Existe?       |  ---  |    013    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Rut Existe?        |  ---  |    014    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Mail Unico?        |  ---  |    015    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
-   Password Coincide?  |  ---  |    016    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
+  Password Coincide? |  ---  |    016    |    ---    |    ---    |
+  -------------------|-------|-----------|-----------|-----------|
 	Modificar Mail     |  ---  |    ---    |    020    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
+  -------------------|-------|-----------|-----------|-----------|
 	Modificar Pass     |  ---  |    ---    |    021    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
-  Subir imagen empresa |  022  |    ---    |    ---    |    ---    |
-    -------------------|-------|-----------|-----------|-----------|
-	
-	
-	
+  -------------------|-------|-----------|-----------|-----------|
+Subir imagen empresa |  022  |    ---    |    ---    |    ---    |
+  -------------------|-------|-----------|-----------|-----------|
+
+
+
 	===================CUENTAS ADMINISTRADORES======================
 	Modificar Cuentas  |  ---  |    ---    |    030    |    ---    |
 	-------------------|-------|-----------|-----------|-----------|
-		*******************SUBS Categoria = Nivel 030*******************
-						   | Crear | Comprobar | Modificar |  Eliminar |
-		-------------------|-------|-----------|-----------|-----------|
-		BANEAR             |  ---  |    ---    |    001    |    ---    |
-		-------------------|-------|-----------|-----------|-----------|
-		DESBANEAR          |  ---  |    ---    |    002    |    ---    |
-		-------------------|-------|-----------|-----------|-----------|
-		VOLVER A CONFIRMAR |  ---  |    ---    |    003    |    ---    |
-		-------------------|-------|-----------|-----------|-----------|
-		CONFIRMAR          |  ---  |    ---    |    004    |    ---    |
-		-------------------|-------|-----------|-----------|-----------|
-		****************************************************************
-	
+	*******************SUBS Categoria = Nivel 030*******************
+					   				 | Crear | Comprobar | Modificar |  Eliminar |
+	-------------------|-------|-----------|-----------|-----------|
+	BANEAR             |  ---  |    ---    |    001    |    ---    |
+	-------------------|-------|-----------|-----------|-----------|
+	DESBANEAR          |  ---  |    ---    |    002    |    ---    |
+	-------------------|-------|-----------|-----------|-----------|
+	VOLVER A CONFIRMAR |  ---  |    ---    |    003    |    ---    |
+	-------------------|-------|-----------|-----------|-----------|
+	CONFIRMAR          |  ---  |    ---    |    004    |    ---    |
+	-------------------|-------|-----------|-----------|-----------|
+	****************************************************************
+
 
 */
 $objDB = new consultas;
@@ -121,36 +121,36 @@ switch ($caso) {
 		$DestinationDirectory	= realpath('./uploads/empresa/'); //specify upload directory ends with / (slash)
 		$Quality 				= 90; //jpeg quality
 		##########################################
-	
+
 		//check if this is an ajax request
 		if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
 			die('This is not an ajax request');
 		}
-		
+
 		// check $_FILES['ImageFile'] not empty
 		if(!isset($_FILES['ImageFile']) || !is_uploaded_file($_FILES['ImageFile']['tmp_name']))
 		{
 				die('Something wrong with uploaded file, something missing!'); // output error when above checks fail.
 		}
-		
+
 		// Random number will be added after image name
-		$RandomNumber 	= rand(0, 9999999999); 
-	
+		$RandomNumber 	= rand(0, 9999999999);
+
 		$ImageName 		= str_replace(' ','-',strtolower($_FILES['ImageFile']['name'])); //get image name
 		$ImageSize 		= $_FILES['ImageFile']['size']; // get original image size
 		$TempSrc	 	= $_FILES['ImageFile']['tmp_name']; // Temp name of image file stored in PHP tmp folder
 		$ImageType	 	= $_FILES['ImageFile']['type']; //get file type, returns "image/png", image/jpeg, text/plain etc.
-	
+
 		//Let's check allowed $ImageType, we use PHP SWITCH statement here
 		switch(strtolower($ImageType))
 		{
 			case 'image/png':
-				//Create a new image from file 
+				//Create a new image from file
 				$CreatedImage =  imagecreatefrompng($_FILES['ImageFile']['tmp_name']);
 				break;
 			case 'image/gif':
 				$CreatedImage =  imagecreatefromgif($_FILES['ImageFile']['tmp_name']);
-				break;			
+				break;
 			case 'image/jpeg':
 			case 'image/pjpeg':
 				$CreatedImage = imagecreatefromjpeg($_FILES['ImageFile']['tmp_name']);
@@ -158,26 +158,26 @@ switch ($caso) {
 			default:
 				die('Unsupported File!'); //output error and exit
 		}
-		
+
 		//PHP getimagesize() function returns height/width from image file stored in PHP tmp folder.
-		//Get first two values from image, width and height. 
+		//Get first two values from image, width and height.
 		//list assign svalues to $CurWidth,$CurHeight
 		list($CurWidth,$CurHeight)=getimagesize($TempSrc);
-		
+
 		//Get file extension from Image name, this will be added after random name
 		$ImageExt = substr($ImageName, strrpos($ImageName, '.'));
 		$ImageExt = str_replace('.','',$ImageExt);
-		
+
 		//remove extension from filename
-		$ImageName 		= preg_replace("/\\.[^.\\s]{3,4}$/", "", $ImageName); 
-		
+		$ImageName 		= preg_replace("/\\.[^.\\s]{3,4}$/", "", $ImageName);
+
 		//Construct a new name with random number and extension.
 		$NewImageName = $ImageName.'-'.$RandomNumber.'.'.$ImageExt;
-		
+
 		//set the Destination Image
 		$thumb_DestRandImageName 	= $DestinationDirectory.'/'.$ThumbPrefix.$NewImageName; //Thumbnail name with destination directory
 		$DestRandImageName 			= $DestinationDirectory.'/'.$NewImageName; // Image with destination directory
-		
+
 		//Resize image to Specified Size by calling resizeImage function.
 		if(resizeImage($CurWidth,$CurHeight,$BigImageMaxSize,$DestRandImageName,$CreatedImage,$Quality,$ImageType))
 		{
@@ -193,9 +193,9 @@ switch ($caso) {
 				echo 'db/uploads/empresa/'.$ThumbPrefix.$NewImageName;
 			#No cierro la conexion por que es un insertado asincrono...
 			}else{
-				die('ERROR insertando imagen a DB');	
+				die('ERROR insertando imagen a DB');
 			}
-	
+
 		}else{
 			die('Resize Error'); //output error
 		}
@@ -212,36 +212,36 @@ case 23: #SUBIR IMAGEN USUARIO
 		$DestinationDirectory	= realpath('./uploads/usuario/'); //specify upload directory ends with / (slash)
 		$Quality 				= 90; //jpeg quality
 		##########################################
-	
+
 		//check if this is an ajax request
 		if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
 			die('This is not an ajax request');
 		}
-		
+
 		// check $_FILES['ImageFile'] not empty
 		if(!isset($_FILES['ImageFile']) || !is_uploaded_file($_FILES['ImageFile']['tmp_name']))
 		{
 				die('Something wrong with uploaded file, something missing!'); // output error when above checks fail.
 		}
-		
+
 		// Random number will be added after image name
-		$RandomNumber 	= rand(0, 9999999999); 
-	
+		$RandomNumber 	= rand(0, 9999999999);
+
 		$ImageName 		= str_replace(' ','-',strtolower($_FILES['ImageFile']['name'])); //get image name
 		$ImageSize 		= $_FILES['ImageFile']['size']; // get original image size
 		$TempSrc	 	= $_FILES['ImageFile']['tmp_name']; // Temp name of image file stored in PHP tmp folder
 		$ImageType	 	= $_FILES['ImageFile']['type']; //get file type, returns "image/png", image/jpeg, text/plain etc.
-	
+
 		//Let's check allowed $ImageType, we use PHP SWITCH statement here
 		switch(strtolower($ImageType))
 		{
 			case 'image/png':
-				//Create a new image from file 
+				//Create a new image from file
 				$CreatedImage =  imagecreatefrompng($_FILES['ImageFile']['tmp_name']);
 				break;
 			case 'image/gif':
 				$CreatedImage =  imagecreatefromgif($_FILES['ImageFile']['tmp_name']);
-				break;			
+				break;
 			case 'image/jpeg':
 			case 'image/pjpeg':
 				$CreatedImage = imagecreatefromjpeg($_FILES['ImageFile']['tmp_name']);
@@ -249,26 +249,26 @@ case 23: #SUBIR IMAGEN USUARIO
 			default:
 				die('Unsupported File!'); //output error and exit
 		}
-		
+
 		//PHP getimagesize() function returns height/width from image file stored in PHP tmp folder.
-		//Get first two values from image, width and height. 
+		//Get first two values from image, width and height.
 		//list assign svalues to $CurWidth,$CurHeight
 		list($CurWidth,$CurHeight)=getimagesize($TempSrc);
-		
+
 		//Get file extension from Image name, this will be added after random name
 		$ImageExt = substr($ImageName, strrpos($ImageName, '.'));
 		$ImageExt = str_replace('.','',$ImageExt);
-		
+
 		//remove extension from filename
-		$ImageName 		= preg_replace("/\\.[^.\\s]{3,4}$/", "", $ImageName); 
-		
+		$ImageName 		= preg_replace("/\\.[^.\\s]{3,4}$/", "", $ImageName);
+
 		//Construct a new name with random number and extension.
 		$NewImageName = $ImageName.'-'.$RandomNumber.'.'.$ImageExt;
-		
+
 		//set the Destination Image
 		$thumb_DestRandImageName 	= $DestinationDirectory.'/'.$ThumbPrefix.$NewImageName; //Thumbnail name with destination directory
 		$DestRandImageName 			= $DestinationDirectory.'/'.$NewImageName; // Image with destination directory
-		
+
 		//Resize image to Specified Size by calling resizeImage function.
 		if(resizeImage($CurWidth,$CurHeight,$BigImageMaxSize,$DestRandImageName,$CreatedImage,$Quality,$ImageType))
 		{
@@ -284,9 +284,9 @@ case 23: #SUBIR IMAGEN USUARIO
 				echo 'db/uploads/usuario/'.$ThumbPrefix.$NewImageName;
 			#No cierro la conexion por que es un insertado asincrono...
 			}else{
-				die('ERROR insertando imagen a DB');	
+				die('ERROR insertando imagen a DB');
 			}
-	
+
 		}else{
 			die('Resize Error'); //output error
 		}
